3c88fb143c215e6e8787817158ef47d7f37c0b3b
gniewko-s <72751239+gniewko-s@users.noreply.github.com>
2022-11-23 18:20:24 +0100

    Add files via upload

--- equilateral.py
+++ equilateral.py
@@ -2,19 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-def satisfy_original(a,b,c,phi,r,tol=1e-6):
+def satisfy_original(a: float,b: float,c: float,phi: float,r: float,tol: float=1e-6) -> bool:
+	"""satisfy_original(a,b,c,phi,r,tol=1e-6) checks if the point (phi,r) saturates the boundary condition for parameters (a,b,c) with tolerance tol"""
 	res = ((a-1-r*np.cos(phi))**2 - 3*r**2*np.sin(phi)**2)**.5 + ( ((b+c)/2 + 2*r*np.cos(phi))**2 - ((b-c)/2)**2 )**.5 - 1
 	return np.abs(res) < tol 
 
-def in_triangle(a,b,c,phi,r):
-	return r <= .6 * min(a-1,b,c) / max(-np.cos(phi),-np.cos(phi+2*np.pi/3),-np.cos(phi-2*np.pi/3))	# .5 to fitt the cut off to triangle
+def in_triangle(a: float,b: float,c: float,phi: float,r: float, cutoff: float = .5) -> bool:
+	"""For the value of cutoff = .5 check if the solution contains in the blue triangle implied by the vetres conditions.
+For higher values of the cutoff parameter, the solution will be drawn also outside the blue triangle"""
+	return r <= cutoff * min(a-1,b,c) / max(-np.cos(phi),-np.cos(phi+2*np.pi/3),-np.cos(phi-2*np.pi/3))
 
-def on_roots(a,b,c,phi,roots):
+def on_roots(a: float,b: float,c: float,phi: float,roots: np.array):
+	"""For the array of four roots calculated for the given phi from the edge condition with parameters a,b,c, put np.NaN for roots non-positive, out of trangle or not satisfying the original conditions and returns their mininum, possibly being a np.NaN value"""
 	roots = [r.real if r.imag == 0 and r>0 and satisfy_original(a,b,c,phi,r) and in_triangle(a,b,c,phi,r) else np.NaN for r in roots] 
 	m = np.nanmin(roots)
 	return m
 
-def arc_polar(a,b,c):
+def arc_polar(a: float,b: float,c: float):
+	"""For given a,b,c returns a function inner
+Function inner returns a solution (1-d np.array) of the corresponding edge condition for a given range of angle (1-d np.array)"""
 	def inner(phi):
 		coeff4 = 9/4; coeff4 = coeff4*np.ones(phi.shape)
 		coeff3 = 3*(a+b+c-1) * np.cos(phi)
@@ -27,6 +33,11 @@ def arc_polar(a,b,c):
 	return inner
 
 def plot(a,b,c):
+	"""For a given values of parameters a,b,c, the function returns the points of the plots:
+* XV,YV - the 1-d numpy arrays of respectively x and y coordinates of the blue triangle arising from the vertex conditions.
+* XE,YE - the 1-d numpy arrays of respectively x and y coordinates of the red triangle arising from the edge conditions.
+* XP,YP - the 1-d numpy arrays of respectively x and y coordinates of the green circle arising from the hessian positivity condition.
+	"""
 	mu = min(a-1,b,c)
 	r = 6**.5 * mu
 	xmax = 3**.5/2*r
@@ -67,6 +78,8 @@ slider1 = Slider(ax = fig.add_axes([0.25, 0.15, 0.65, 0.03]), label='$a+b+c$', v
 slider2 = Slider(ax = fig.add_axes([0.25, 0.1, 0.65, 0.03]), label='b+c', valmin=0, valmax=2, valinit=si2)
 slider3 = Slider(ax = fig.add_axes([0.25, 0.05, 0.65, 0.03]), label='b-c', valmin=0, valmax=2, valinit=si3)
 
+# Functions updating the values of parameters when sliders change, taking care of positivity of parameters and $2-a < \sqrt{bc}$ to be satisfied.
+
 def update_1(val):
 	s1 = slider1.val; s2 = slider2.val; s3 = slider3.val
 	if (s2**2-s3**2)**.5/2+s1-s2 < 2:
@@ -96,6 +109,8 @@ def update_3(val):
 		s1 = 2 + s2 - (s2**2-s3**2)**.5/2
 		slider1.set_val(s1)
 	redraw(s1,s2,s3)
+
+# generate new data for plots and redraw; a common callback of the above slider functions.
 
 def redraw(s1,s2,s3):
 	global V,E
